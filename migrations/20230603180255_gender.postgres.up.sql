cdef8eacfce0dc20f4d5e0eda6213191f56c4f05
riyan-eng <fbriyant11@gmail.com>
2023-06-04 01:10:18 +0700

    update: migration

--- migrations/20230603180255_gender.postgres.up.sql
+++ migrations/20230603180255_gender.postgres.up.sql
@@ -1,9 +1,11 @@
-create table tasks(
+create table genders(
     id uuid not null DEFAULT gen_random_uuid(),
-    name varchar(255) not null,
-    detail text,
+    code varchar(255) unique,
+    name varchar(255),
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp,
     
     primary key(id)
-)
+);
+
+insert into genders (code, name) values ('L', 'Laki-laki'), ('P', 'Perempuan');
--- migrations/20230603180403_user.postgres.up.sql
+++ migrations/20230603180403_user.postgres.up.sql
@@ -1,19 +1,13 @@
-create table genders(
-    id uuid not null DEFAULT gen_random_uuid(),
-    code varchar(255) unique,
-    name varchar(255),
-    primary key(id)
-);
-
-insert into genders (code, name) values ('L', 'Laki-laki'), ('P', 'Perempuan');
-
 create table user_datas(
     id uuid not null DEFAULT gen_random_uuid(),
     gender uuid,
     name varchar(255),
     address text,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+
     primary key(id),
-    constraint fk_gender foreign key(gender) references genders(id)
+    constraint fk_gender foreign key(gender) references genders(id) on update cascade
 );
 
 create table users(
@@ -23,6 +17,9 @@ create table users(
     email varchar(255) unique nulls not distinct,
     phone varchar(255) unique nulls not distinct,
     user_data uuid,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
+
     primary key(id),
-    constraint fk_user_data foreign key(user_data) references user_datas(id)
+    constraint fk_user_data foreign key(user_data) references user_datas(id) on update cascade
 );
